33c12fcb4c129633e6f0943a6f6638e721134dd4
Tom Gilbey <w22002938@northumbria.ac.uk>
2024-11-13 12:52:20 +0000

    Testing burger menu

--- functions.php
+++ functions.php
@@ -201,19 +201,9 @@ HTML;
 
 function makePageEnd() {
     return <<<HTML
-        <script>
-            document.addEventListener('DOMContentLoaded', () => {
-                const burger = document.querySelector('.navbar-burger');
-                const menu = document.querySelector('.navbar-menu');
-                
-                burger.addEventListener('click', () => {
-                    burger.classList.toggle('is-active');
-                    menu.classList.toggle('is-active');
-                });
-            });
-        </script>
+    <script src="burger.js"></script>
     </body>
-</html>
+    </html>
 HTML;
 }
 

bd9094452dbd8fde162137ec6771195b4cf22e1d
Tom Gilbey <w22002938@northumbria.ac.uk>
2024-11-13 00:16:03 +0000

    Testing organisation link

--- functions.php
+++ functions.php
@@ -134,6 +134,9 @@ function logOut(){//Function to log out of account
 }
 
 function makeNavMenu($navMenuHeader, array $links) {
+    // Check session variables for debugging
+    var_dump($_SESSION);
+
     $output = <<<HTML
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
@@ -156,34 +159,26 @@ HTML;
     }
 
     if (check_login()) {
-        // Fetch userID from the session
         $userID = get_session('userID');
-        $dbConn = getConnection();
+        var_dump($userID); // Debug: Check if userID is set
+        
+        $userOrganisationID = getUserOrganisation($userID);
+        var_dump($userOrganisationID); // Debug: Check organisationID retrieved for the user
 
-        // Query to get the organisationID for the logged-in user
-        $sql = "SELECT organisationID FROM userTable WHERE userID = :userID";
-        $stmt = $dbConn->prepare($sql);
-        $stmt->execute([':userID' => $userID]);
-        $organisation = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($organisation) {
-            // Get the teamLeaderID for this organisation
-            $organisationID = $organisation['organisationID'];
-            $sql = "SELECT teamLeaderID FROM organisationTable WHERE organisationID = :organisationID";
-            $stmt = $dbConn->prepare($sql);
-            $stmt->execute([':organisationID' => $organisationID]);
-            $orgDetails = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($orgDetails && $orgDetails['teamLeaderID'] == $userID) {
+        if ($userOrganisationID) {
+            $organisationDetails = getOrganisation($userOrganisationID);
+            var_dump($organisationDetails); // Debug: Confirm teamLeaderID
+            
+            if ($organisationDetails && $organisationDetails['teamLeaderID'] == $userID) {
                 // Display "Manage Organisation" link if user is the team leader
-                $output .= "<a class=\"navbar-item\" href='manageOrganisation.php?id=" . $organisationID . "'>Manage Organisation</a>\n";
+                $output .= "<a class=\"navbar-item\" href='manageOrganisation.php?id=" . $userOrganisationID . "'>Manage Organisation</a>\n";
             }
         }
-        
-        // Display logout link if logged in
+
+        // Add logout option if logged in
         $output .= "<a class=\"navbar-item\" href='logout.php'>Logout</a>\n";
     } else {
-        // Display login and register links if not logged in
+        // Add login and register options if not logged in
         $output .= "<a class=\"navbar-item\" href='loginForm.php'>Login</a>\n";
         $output .= "<a class=\"navbar-item\" href='register.php'>Register</a>\n";
     }
@@ -196,8 +191,6 @@ HTML;
 
     return $output;
 }
-
-
 
 
 

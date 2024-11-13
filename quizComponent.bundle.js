a2d11436dc3b090521f70f671ad54a3aa4208734
Tom Gilbey <w22002938@northumbria.ac.uk>
2024-11-13 14:30:41 +0000

    capitalisation error

--- quizComponent.bundle.js
+++ quizComponent.bundle.js
@@ -9,7 +9,7 @@ const quizData = window.quizData;
 
 const rootElement = document.getElementById('quiz-root');
 if (rootElement) {
-    ReactDOM.render(<QuizComponent quizData={quizData} />, rootElement); // Use render instead of createRoot
+    ReactDOM.render(<quizComponent quizData={quizData} />, rootElement); // Use render instead of createRoot
 } else {
     console.error("No element with id 'quiz-root' found.");
 }

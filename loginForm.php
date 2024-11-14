1a854c7d97b590d1d5725ffc570aacdfe2100d84
Tom Gilbey <w22002938@northumbria.ac.uk>
2024-11-14 10:56:25 +0000

    Styling Login form using bulma

--- loginForm.php
+++ loginForm.php
@@ -5,19 +5,41 @@ echo makePageStart("Login");
 echo makeNavMenu("CyberPath");
 ?>
 
-<form id="loginForm" action="loginProcess.php" method="post">
-    <fieldset>
-        <legend>Login</legend>
+<div class="container mt-5">
+    <div class="columns is-centered">
+        <div class="column is-half">
+            <form id="loginForm" action="loginProcess.php" method="post" class="box">
+                <fieldset>
+                    <legend class="title is-4 has-text-centered">Login</legend>
 
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" >
+                    <!-- Username field -->
+                    <div class="field">
+                        <label for="username" class="label">Username</label>
+                        <div class="control">
+                            <input type="text" name="username" id="username" class="input" placeholder="Enter your username">
+                        </div>
+                    </div>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" >
+                    <!-- Password field -->
+                    <div class="field">
+                        <label for="password" class="label">Password</label>
+                        <div class="control">
+                            <input type="password" name="password" id="password" class="input" placeholder="Enter your password">
+                        </div>
+                    </div>
 
-        <input type="submit" value="Log in" >
-    </fieldset>
-</form> <!-- Form for login function -->
+                    <!-- Submit button -->
+                    <div class="field">
+                        <div class="control">
+                            <button type="submit" class="button is-primary is-fullwidth">Log in</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
+
 
 <?php
 echo makeFooter("Footer");

478ca55f476f501c102bd1013463be74f8e47647
Matthew <mattyloc1@gmail.com>
2024-11-16 19:37:41 +0000

    effect

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['username'])) {
     echo "<section class='hero is-info is-bold'>";
     echo "<div class='hero-body'>";
     echo "<div class='container'>";
-    echo "<h1 class='title is-1'>Welcome back, " . htmlspecialchars($_SESSION['username']) . "!</h1>";
+    echo "<div id='typeWriterEffect'><h1 class='title is-1'>Welcome back, " . htmlspecialchars($_SESSION['username']) . "!</h1>";
     echo "</div>";
     echo "</div>";
     echo "</section>";
@@ -59,7 +59,7 @@ if (isset($_SESSION['username'])) {
     echo "<section class='hero is-info is-bold'>";
     echo "<div class='hero-body'>";
     echo "<div class='container'>";
-    echo "<div id=welcomeMessage'>='title is-1'>Welcome to CyberPath!</h1>";
+    echo "<h1 class='title is-1'>Welcome to CyberPath!</h1>";
     echo "</div>";
     echo "</div>";
     echo "</section>";

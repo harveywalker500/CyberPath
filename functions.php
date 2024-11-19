c4c25cd0bc62803b8e27d6d7743d14752b6e3886
Tom Gilbey <w22002938@northumbria.ac.uk>
2024-11-19 00:26:21 +0000

    css

--- functions.php
+++ functions.php
@@ -33,7 +33,6 @@ function makePageStart($title, $stylesheet)
         <script src="DataTables/datatables.min.js"></script>
         <link rel="stylesheet" href="$stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="stylesheet.css">
     </head>
     <body>
 HTML;

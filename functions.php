fd461a6b6af35aca9ca74a4cccab7af1a8d50e08
Tom Gilbey <w22002938@northumbria.ac.uk>
2024-11-19 00:28:38 +0000

    change to see if it was messging with next_question

--- functions.php
+++ functions.php
@@ -29,8 +29,7 @@ function makePageStart($title, $stylesheet)
         <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- jQuery CDN -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-        <script src="dev2.js"></script>
-        <script src="DataTables/datatables.min.js"></script>
+        
         <link rel="stylesheet" href="$stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
